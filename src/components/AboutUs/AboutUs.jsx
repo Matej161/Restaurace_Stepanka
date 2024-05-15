432cf2b1df7c727a6fc5148008cee7b1a697623c
matej161 <matej.kulisek16@seznam.cz>
2024-05-15 10:32:49 +0200

    batn

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -15,23 +15,54 @@ import {
   Grid,
   Drawer,
 } from "@mui/material";
+import "./AboutUs.css";
 
 export default function AboutUs() {
   return (
     <>
-      <Grid container sx={{background: "black"}}>
-        <Grid xs={12} md={6}>
+      <Grid container sx={{ background: "black" }}>
+        <Grid bgcolor={"#dadada"} xs={12} md={6}>
           <Box>
             <img width={"100%"} src={"./src/images/moderni_jidlo.jpg"} />
           </Box>
         </Grid>
-        <Grid xs={12} md={6}>
+        <Grid
+          xs={12}
+          md={6}
+          container
+          alignItems={"center"}
+          sx={{ background: "white" }}
+          pl={3}
+          justifyContent={"center"}
+        >
           <Box>
-            <h1>MENU</h1>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
-            nesciunt, possimus quas accusamus saepe neque laudantium error
-            numquam cum repudiandae perspiciatis molestias aspernatur natus
-            doloremque sequi nemo quibusdam vero veniam.
+            <Typography variant="h4" fontFamily={"freeman"}>
+              MENU
+            </Typography>
+            <Typography variant="body1" fontFamily={"freeman"}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
+              nesciunt, possimus quas accusamus saepe neque laudantium error
+              numquam cum repudiandae perspiciatis molestias aspernatur natus
+              doloremque sequi nemo quibusdam vero veniam.
+            </Typography>
+            <Box ml={2} mt={2}>
+              {" "}
+              {/*<Button
+                sx={{
+                  ":hover": {
+                    bgcolor: "green",
+                    color: "white",
+                  },
+                  ":active": {
+                    bgcolor: "yellow",
+                  },
+                  bgcolor: "#dadada",
+                }}
+              >
+                AHOJ
+              </Button>*/}
+              <button id="menu-button"> Button </button>
+            </Box>
           </Box>
         </Grid>
       </Grid>
